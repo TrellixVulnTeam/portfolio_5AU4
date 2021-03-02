8dcb16e496b63ee826eb88773ddb316b0e63d951
Sebastian <sebgraph7@gmail.com>
2021-03-02 18:52:32 -0500

    menu

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+import hamburguerMenu from "./modules/hamburger_menu.js";
 import portfolioFilter from "./modules/portfolio_filter.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     portfolioFilter();
+    hamburguerMenu(".header__btn-responsive", ".header__navigation", ".header__link");
 
 });
---
+++ js/modules/hamburger_menu.js
@@ -0,0 +1,18 @@
+const d = document;
+
+export default function hamburguerMenu(menuBtn, menu, menuLink){
+
+    d.addEventListener("click", (e)=> {
+        if(e.target.matches(menuBtn) || e.target.matches(`${menuBtn} *`)){
+            d.querySelector(menu).classList.toggle("is-active");
+            d.querySelector(menuBtn).classList.toggle("is-active");
+        }
+
+        if(e.target.matches(menuLink)) {
+          d.querySelector(menu).classList.remove("is-active");
+          d.querySelector(menuBtn).classList.remove("is-active");
+
+        }
+    });
+
+}
