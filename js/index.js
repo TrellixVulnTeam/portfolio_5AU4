366fc3f58146770c3718f478a994d5350432271c
Sebastian <sebgraph7@gmail.com>
2021-03-05 19:47:37 -0500

    nav bar icons

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 import hamburguerMenu from "./modules/hamburger_menu.js";
 import portfolioFilter from "./modules/portfolio_filter.js";
+import tabBar from "./modules/tab_bar.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     portfolioFilter();
     hamburguerMenu(".header__btn-responsive", ".header__menu", ".header__link");
+    tabBar(".header__item");
+
 
 });
---
+++ js/modules/tab_bar.js
@@ -0,0 +1,16 @@
+const d = document;
+
+export default function tabBar(container) {
+
+
+
+/*         d.addEventListener("click", (e)=> {
+        if(e.target.matches(container) || e.target.matches(`${container} *`)){
+            e.preventDefault();
+            d.querySelector(container).classList.toggle("menu-active");
+        }
+
+    });  */
+
+
+}
