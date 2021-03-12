199b1448da4e453eaed1099a9e20d0bf562767b4
Sebastian <sebgraph7@gmail.com>
2021-03-12 08:52:21 -0500

    modal ajustado

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -9,18 +9,44 @@ export default function modal(){
         const modalToogle = e.target.closest('.portfolio-link');
 
         if(! modalToogle) return
-        const modal = modalToogle.parentNode.nextElementSibling;
-        const closeBtn =modal.querySelector('.modal__close');
+        const modal    = modalToogle.parentNode.nextElementSibling;
+        const closeBtn = modal.querySelector('.modal__close');
 
         
         // to open  the modal
-        modal.classList.remove('display-none');
+        const modalOpen = _ => {
+            modal.classList.remove('display-none');
+            modal.style.animation = 'modalIn 500ms forwards';
+            d.body.style.overflowY = 'hidden';
+        } 
 
         // to close the modal
-        closeBtn.addEventListener('click', el => {
+        const modalClose = _ => {
             modal.classList.add('display-none');
+            modal.removeEventListener('animationend', modalClose);
+
+        }
+
+        // to animate close button
+        closeBtn.addEventListener('click', el => {
+            modal.style.animation = 'modalOut 500ms forwards';
+            modal.addEventListener('animationend', modalClose);
+            d.body.style.overflowY = 'scroll';
 
         });
+
+        // to use scape keyboard
+        d.addEventListener('keydown', e => {
+            if ( e.key === 'Escape') {
+                modal.style.animation = 'modalOut 500ms forwards';
+                modal.addEventListener('animationend', modalClose);
+                d.body.style.overflowY = 'scroll';
+
+            }
+
+        });
+
+        modalOpen();
 
     });
 }
